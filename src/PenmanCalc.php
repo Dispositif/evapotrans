ae2034d01b631628681c76319225c38ddb0021df
Phil M <dispositif@gmail.com>
2019-10-18 12:06:48 +0200

    Update copyright

--- src/PenmanCalc.php
+++ src/PenmanCalc.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * This file is part of dispositif/evapotrans library
+ * 2014-2019 (c) Philippe M. <dispositif@gmail.com>
+ * For the full copyright and license information, please view the LICENSE file
+ */
 
 namespace Evapotrans;
 
