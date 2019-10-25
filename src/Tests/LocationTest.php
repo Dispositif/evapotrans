29933ce797bfaa5deb947b8eab9dd07747d21c13
Phil M <dispositif@gmail.com>
2019-10-25 16:35:45 +0200

    Style

--- src/Tests/LocationTest.php
+++ src/Tests/LocationTest.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of dispositif/evapotrans library
  * 2014-2019 (c) Philippe M. <dispositif@gmail.com>
- * For the full copyright and license information, please view the LICENSE file
+ * For the full copyright and license information, please view the LICENSE file.
  */
 
 namespace Evapotrans\Tests;
